986e1a00fc00d4580817ff3f6376a9d84750bfed
VishruthaKannan <vishruthakannan6@gmail.com>
2025-04-09 12:21:00 +0530

    Add files via upload

---
+++ TechShop/Exceptions/CustomExceptions.py
@@ -0,0 +1,4 @@
+class InvalidDataException(Exception): pass
+class InsufficientStockException(Exception): pass
+class IncompleteOrderException(Exception): pass
+class PaymentFailedException(Exception): pass
